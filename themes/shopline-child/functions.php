c186f69e14f618968ac4c72732bc0e6566e19db3
mariana <mariana.j.ambrosetti@gmail.com>
2018-08-30 19:27:01 -0300

    Chil theme + Custom CSS

---
+++ themes/shopline-child/functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+// css del tema padre
+	
+	function dl_enqueue_style_child() {
+
+		wp_register_style( 'parent-style', get_template_directory_uri().'/style.css', null, null, 'screen');
+		wp_register_style( 'custom-style', get_theme_file_uri(). '/assets/css/custom_style.css' , null, null, 'screen' );
+
+		wp_enqueue_style('parent-style');
+		wp_enqueue_style( 'custom-style');
+	}
+
+	add_action( 'wp_enqueue_scripts', 'dl_enqueue_style_child' );
+
+
+
